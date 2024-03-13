b8bd8e1f7c4c7522160431226ecf331f78d151ff
Gal Zohar <galz@ai21.com>
2024-03-14 00:15:49 +0200

    added typescript dynamic checks to build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import typescriptCheckerPlugin from "vite-plugin-checker";
+import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), typescriptCheckerPlugin({ typescript: true })],
   build: {
-    outDir: "./build"
-  }
-})
+    outDir: "./build",
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: fileURLToPath(new URL("./src/components", import.meta.url)) }],
+  },
+});
